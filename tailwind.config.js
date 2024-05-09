51fd704ba67dc0d62a2408243d64d3c995cb5e2c
JuaneProd <juane@juaneprod.com>
2024-05-10 02:28:36 +0300

    Main

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: ["./*.html", "./*.js"],
-    darkMode: "media",
+    darkMode: "class",
     safelist: ["isToggled"],
     theme: {
       fontFamily: {
@@ -18,6 +18,12 @@ module.exports = {
                 },
             },
         },
+        extend: {
+            colors: {
+              customBlue: '#3854F2',
+              customDark: '#121212',
+            },
+          },      
         
     },
     plugins: [
